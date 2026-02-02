cf121ee398df63fbd40fa6c86d51ad9bdb712620
David Crespo <david.crespo@correounivalle.edu.co>
2026-02-02 00:24:09 -0500

    merged multiplaer

--- GGJ2026/Assets/Hemmi/Spricts/TitleManager.cs
+++ GGJ2026/Assets/Hemmi/Spricts/TitleManager.cs
@@ -7,8 +7,10 @@ using UnityEngine.UI;
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] string m_nextSceneName = "GameScene";
+    [SerializeField] string multiplayerScene = "MultiplayerWaiting";
     [SerializeField] Image m_fadeImage;
     [SerializeField] float m_fadeTime = 2.0f;
+    bool normalGame = false;
     
     AudioSource m_audioSource;
     int m_isFadeIn = 0;
@@ -31,10 +33,20 @@ public class TitleManager : MonoBehaviour
         FadeIn();
         if(m_isFadeIn == -1)
         {
+            if(normalGame)
             SceneManager.LoadScene(m_nextSceneName);
+            else
+            SceneManager.LoadScene(multiplayerScene);
         }
     }
     public void OnCilckButton()
+    {
+        m_audioSource.Play();
+        m_fadeImage.enabled = true;
+        m_isFadeIn = 1;
+        normalGame = true;
+    }
+    public void OnCilckMultiplayerButton()
     {
         m_audioSource.Play();
         m_fadeImage.enabled = true;
